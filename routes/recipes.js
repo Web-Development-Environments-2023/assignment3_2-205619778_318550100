1f73d009022d3656ebdb085b04192b3a1fd3db33
BIMAOR6 <73882463+BIMAOR6@users.noreply.github.com>
2023-05-28 22:27:23 +0300

    recipe complete

--- routes/recipes.js
+++ routes/recipes.js
@@ -4,6 +4,38 @@ const recipes_utils = require("./utils/recipes_utils");
 
 router.get("/", (req, res) => res.send("im here"));
 
+/**
+ * This path returns a full details of a recipe by its id
+ */
+router.get("/RecipeDetailsAPI/:recipeId", async (req, res, next) => {
+  try {
+    const full_recipe = await recipes_utils.getFullRecipeDetails(req.params.recipeId);
+    res.send(full_recipe);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * This path returns the recipes for an Unauthorized USER that the user search by cusine, diet or intolerance
+ */
+router.get('/search', async (req,res,next) => {
+  try {
+    let m_recipes = await recipes_utils.getFilteredSearchRecipes(req.query.query, req.query.number, req.query.cuisine, req.query.diet, req.query.intolerance,req.query.sort);
+    res.send(m_recipes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/random", async (req, res, next) => {
+  try {
+    let random_three_recipe = await recipes_utils.getRandomThreeRecipes();
+    res.send(random_three_recipe);
+  } catch (error) {
+    next(error);
+  }
+});
 
 /**
  * This path returns a full details of a recipe by its id
@@ -17,4 +49,5 @@ router.get("/:recipeId", async (req, res, next) => {
   }
 });
 
-module.exports = router;
+
+module.exports = router;  
--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const { response } = require("express");
 const api_domain = "https://api.spoonacular.com/recipes";
 
 
@@ -36,10 +37,123 @@ async function getRecipeDetails(recipe_id) {
         
     }
 }
+//get random recipes from spooncolar api
+async function getRandomRecipes(){
+    const response = await axios.get(`${api_domain}/random`, {
+        params: {
+            limitLicense: true,
+            number: 5,
+            apiKey: process.env.spooncular_apiKey
+        }
+    }); 
+    return response;
+}
 
+//extract the details from list of recipes
+function extractRecipesDetails(recipes_info){
+    return recipes_info.map((recipe_info) => {
+        let data = recipe_info;
+        if (recipe_info.data){
+            data = recipe_info.data;
+        }
+        let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = data;
+
+        return {
+            id: id,
+            title: title,
+            readyInMinutes: readyInMinutes,
+            image: image,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree,      
+        }
+    })
+}
+
+//get random recipes and filter them that they have at least image and instruction
+async function getRandomThreeRecipes(){
+    let random_recipes = await getRandomRecipes();
+    let filter_random_recipes = random_recipes.data.recipes.filter((random) => (random.instructions != "") && (random.image && random.image != ""));
+    if (filter_random_recipes.length < 3) {
+        return getRandomThreeRecipes();
+    }
+    return extractRecipesDetails([filter_random_recipes[0], filter_random_recipes[1], filter_random_recipes[2]]);
+}
+
+
+async function getFullRecipeDetails(recipe_id) {
+    let recipe_info = await getRecipeInformation(recipe_id);
+    let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree,analyzedInstructions,extendedIngredients,servings} = recipe_info.data;
+    let ingredients_dict = [];
+    extendedIngredients.map((element) => ingredients_dict.push({
+        name: element.name,
+        amount: element.amount,
+    }))
+   
+        return {
+            id: id,
+            title: title,
+            readyInMinutes: readyInMinutes,
+            image: image,
+            popularity: aggregateLikes,
+            vegan: vegan,
+            vegetarian: vegetarian,
+            glutenFree: glutenFree,
+            extendedIngredients: ingredients_dict,
+            analyzedInstructions: analyzedInstructions,  
+            servings: servings,
+       
+        } 
+}
+
+// get recipe details from spooncular API
+async function getFilteredSearchRecipes(query, number, cuisine, diet, intolerance,sort){
+    if(number === undefined){
+        number=5;
+    }
+    let searchRes = await getRecipesFromSearch(query, number, cuisine, diet, intolerance,sort) ;
+    let filterdSearchRes = searchRes.results.filter((random)=>(random.analyzedInstructions.length != 0))
+    return extractRecipesDetails(filterdSearchRes);
+}
+
+/**get the recipes from the API spooncular
+ number: if not choosen send default 5 
+ query: the recipe name**/
+async function getRecipesFromSearch(query, number, cuisine, diet, intolerance,sort) { 
+    let search_url= `${api_domain}/complexSearch/?query=${query}`
+    if(cuisine !== undefined){
+        search_url = search_url + `&cuisine=${cuisine}`
+    }
+    if(diet !== undefined){
+        search_url = search_url + `&diet=${diet}`
+    }
+    if(intolerance !== undefined){
+        search_url = search_url + `&intolerance=${intolerance}`
+    }
+    if(sort !== undefined){
+        search_url = search_url + `&sort=${sort}`
+    }
+    search_url = search_url + `&instructionsRequired=true&addRecipeInformation=true` 
+    search_url = search_url + `&number=${number}`
+    console.log(search_url);
+    const response = await axios.get(search_url,{
+        
+        params: {
+            apiKey: process.env.spooncular_apiKey
+        }
+    });
+    return response.data;
+
+}
+
+// https://api.spoonacular.com/recipes/complexSearch?query=pasta&maxFat=25&number=2
 
 
 exports.getRecipeDetails = getRecipeDetails;
+exports.getRandomThreeRecipes = getRandomThreeRecipes;
+exports.getFullRecipeDetails = getFullRecipeDetails;
+exports.getFilteredSearchRecipes =getFilteredSearchRecipes;
 
 
 
